e58b5ddfe83e4e659d96d995bcbf074d71d02af7
imamdtechnolife <imamdtechnolife@gmail.com>
2019-10-03 20:57:42 +0800

    training 6

--- public/dom/sandbox.js
+++ public/dom/sandbox.js
@@ -1,3 +1,21 @@
-const ikeh = document.querySelectorAll('p');
-
+let ikeh = document.querySelectorAll('div.name')
 console.log(ikeh);
+
+// ikeh.setAttribute('Imam Afriyadi', 'Antony Stark');
+
+let dragon = document.querySelectorAll('p');
+
+dragon.forEach(element => {
+    console.log(element);
+     element.innerHTML += " hebaat!";
+});
+
+
+const yonkou = ['akagami','shirohige','big mam','kaidou'];
+const kaidou = document.querySelector('.kakuju')
+
+yonkou.forEach(element => {
+    console.log(element)
+    kaidou.innerHTML += `<p>${element}</p>`;
+})
+
