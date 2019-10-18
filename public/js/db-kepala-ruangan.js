cfdf88bb17bcaca2f87c058f5b800a972c16f715
imamdtechnolife <imamdtechnolife@gmail.com>
2019-10-18 08:22:45 +0800

    bug fix

--- public/js/db-kepala-ruangan.js
+++ public/js/db-kepala-ruangan.js
@@ -27,24 +27,118 @@ dbku.enablePersistence()
     }
   });
 
-  // add new recipe
-const form = document.querySelector('form');
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  
-  if (uid === admin_igd){
 
+
+  // add new recipe
+// const form = document.querySelector('form');
+// form.addEventListener('checkbox-alat', evt => {
+//   evt.preventDefault();
+  
+//   // if (uid === admin_igd){
+//   //   if(){
+    
+// });
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
   }
-  const recipe = {
-    nama_alat: form.title.value,
-    jenis_alat: form.ingredients.value,
-    kategori_alat: selSelect.value
+}
+
+ready(() => {
+  let selection = {};
+  let checkboxElems = document.querySelectorAll("div");
+  let toolsTotal = document.getElementById('total');
+  let listTools = document.getElementById('list-karu');
+
+  console.log(checkboxElems);
+
+  for(let i = 0; i < checkboxElems.length; i++){
+    checkboxElems[i].addEventListener("click", itemClicked)
+  }
+
+  function itemClicked(e){
+    if(e.target.checked){
+      selection[e.target.id] = {
+        nama_alat : 'dragon',
+        id_alat : e.target.id
+      };
+    } else {
+      delete selection[e.target.id];
+    }
   };
 
-  db.collection('alkes').add(recipe)
-    .catch(err => console.log(err));
+  let result = [];
+  let total = 0;
 
-  form.title.value = '';
-  form.ingredients.value = '';
-  
+  for (var key in selection){
+    var listItem = "<li>" + selections[key].value + "</li>";
+    result.push(listItem);
+  }
+  console.log(listItem); 
+
+  listTools.innerHTML = result;
+  // var checkbox = document.querySelector("checkbox");
+//   var checkbox = document.querySelector("checkbox");
+// if(checkbox){
+//   checkbox.addEventListener("change", function() {
+//     if(this.checked) {
+//         // Checkbox is checked..
+//         console.log("running")
+//     } else {
+//         // Checkbox is not checked..
+//     }
+// });
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//  let dragon =  document.querySelector('#checkbox').addEventListener('change', function() {
+//    if(dragon){
+//     if(this.checked) {
+//       // Checkbox is checked..
+//       console.log("running")
+//   } else {
+//       // Checkbox is not checked..
+//   };
+//    }
+   
+//   });
+// });
+
+
+// const checkbox = document.querySelectorAll("checkbox");
+
+//     checkbox.addEventListener('change', function(e) {
+//     if(checkbox.checked) {
+//         // Checkbox is checked.. 
+//         console.log('jalan');
+//         const alkes = {
+//           nama_alat: form.nama-alat.value,
+//           jenis_alat: form.jenis-alat.value,
+//           kategori_alat: form.kategori_alat.value,
+//           jumlah_alat: form.jumlah-alat.value
+//         };
+      
+//         db.collection('igd').add(recipe)
+//           .catch(err => console.log(err));
+//           console.log("data save successfull!")
+//     } else {
+//         // Checkbox is not checked..
+//         // const removeRecipe = (id) => {
+//         //   const recipe = document.querySelector(`.recipe[data-id=${id}]`);
+//         //   recipe.remove();
+//         // };
+//         console.log("run");
+//     }
+//   });
+
+
+// form.checkbox.addEventListener('chh', evt => {
+//   //evt.preventDefault();
+    
+//   console.log("checked running!")
+     
+// })
+
--- public/js/ui-kepala-ruangan.js
+++ public/js/ui-kepala-ruangan.js
@@ -5,23 +5,28 @@ const renderAlkes = (data, id) => {
         <img src="/img/tools.png" alt="recipe thumb">
         <div class="recipe-details">
           <div class="recipe-title">
-            <label>                
-            <input type="checkbox" value="${data.nama_alat}">
-            <span class="checkmark left"></span>
-            ${data.nama_alat}
-            </label>
+            <label>
+              ${data.nama_alat}
+              <input id="${id}" type="checkbox" name="checkbox" value="${data.nama_alat}">
+              <span class="checkmark left"></span>
+            </label> 
           </div>
-          <div class="recipe-ingredients">${data.jenis_alat}</div>
-          <div type='checkbox' id='data'></a>
-        </div>
-        <div class="recipe-delete">
-            <input class="amount right" placeholder="Jumlah" data-id="${id}"></input>
-            <label for="amount"></label>
+          <div class="jenis-alat" id="jenis-alat">
+            Jenis alat       : ${data.jenis_alat}
+          </div>
+          <div class="kategori-alat" id="kategori-alat">
+            Kategori alat : ${data.kategori_alat}
+          </div>
+          <div class="jumlah-alat">
+              <input class="amount right" id="jumla-alat" placeholder="Jumlah" data-id="${id}"></input>
+              <label for="amount"></label>
+          </div>
         </div>
       </div>
       
     `;
-    listAlkes.innerHTML += html;
-    console.log('render alkes success!');
+    // listAlkes.innerHTML += html;
+    listAlkes.insertAdjacentHTML( 'beforeend', html );
+    
   
   };
